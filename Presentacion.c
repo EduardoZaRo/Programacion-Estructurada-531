1391ad00bfdf4e1c1fa82a91ab7986e9f770e913
EduardoZaRo <71353005+EduardoZaRo@users.noreply.github.com>
2021-09-07 08:23:08 -0700

    Introduccion a C

---
+++ Presentacion.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/*Zavala Roman Irvin Eduardo 531
+Tarjeta de presentacion*/
+
+int main(void){
+	
+printf("  ___                                _____                             _                     \n");
+printf(" |   |  _ __  __   __  _   _ __     |__  /   __  _  __   __   __  _   | |   __  _            \n");//Impreso si se ve bien
+printf("  | |  | /__| \\ \\ / / | | | /_ \\      / /   / _\\| | \\ \\ / /  / _\\| |  | |  / _\\| |   \n");
+printf("  | |  | |     \\ V /  | | | | | |    / /_  ( (_)  |  \\ V /  ( (_)  |  | | ( (_)  |         \n");
+printf(" |___| |_|      \\_/   |_| |_| |_|   /____|  \\__/|_|   \\_/    \\__/|_|  |_|  \\__/|_|      \n\n");
+
+printf("     U N I V E R S I D A D  A U T O N O M A  D E  B A J A  C A L I F O R N I A               \n\n");
+printf(" Ingeniero en Computacion                                            661 155 4444            \n\n");//Numero y domicilio inventado
+printf("                     Calle Pancho Barraza Ampliacion Benito Perez                            \n\n");
+printf("                       Pasatiempos:Jugar videojuegos y ajedrez                               \n\n");
+
+
+                                              
+	return 0;
+}
